fff73c3d35143c7eaad56686377082a9d1ecefeb
AhmedNkB <elnaqeeb>
2019-11-26 01:35:36 +0200

    new scribt

---
+++ scribts/scribt.js
@@ -0,0 +1,13 @@
+const b1= document.getElementById("B1");
+b1.onclick=function()
+{
+    alert("wlecome");
+}
+
+const b2=document.getElementById("B2").onclick;
+b2=function()
+{
+document.getElementById("tit").innerHTML="gooooooooooogle"
+} 
+
+}
